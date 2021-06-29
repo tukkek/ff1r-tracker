375abf1bb7691d98d5e2ea293b50ec0edddd3314
Alex Henry <tukkek@gmail.com>
2021-06-29 01:14:16 -0300

    Hint tracker: area autocompletion

--- src/view/hints.js
+++ src/view/hints.js
@@ -1,20 +1,28 @@
 import * as item from '../model/item.js'
+import * as area from '../model/area.js'
 
 const TRACKER=document.querySelector('#hinttracker')
 const HINTS=TRACKER.querySelector('#hints')
 const HINT=HINTS.querySelector('template.hint').content.children[0]
-const NAMES=HINTS.querySelector('datalist#itemnames')
+const ITEMS=HINTS.querySelector('datalist#itemnames')
+const AREAS=HINTS.querySelector('datalist#itemnames')
 const OPEN=document.querySelector('button#openhints')
 const CLOSE=TRACKER.querySelector('button.close')
 
+function register(option,list){
+  let o=document.createElement('option')
+  o.value=option
+  list.appendChild(o)
+}
+
 export function setup(){
+  for(let a of area.acts.flatMap(a=>a)) register(a.name,AREAS)
   for(let i of item.items){
+    register(i.name,ITEMS)
     let div=HINT.cloneNode(true)
-    div.querySelector('input.item').setAttribute('list',NAMES.id)
+    div.querySelector('input.item').setAttribute('list',ITEMS.id)
+    div.querySelector('input.area').setAttribute('list',ITEMS.id)
     HINTS.appendChild(div)
-    let name=document.createElement('option')
-    name.value=i.name
-    NAMES.appendChild(name)
   }
   OPEN.onclick=()=>TRACKER.classList.toggle('hidden')
   CLOSE.onclick=()=>TRACKER.classList.toggle('hidden')
